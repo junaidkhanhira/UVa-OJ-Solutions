4db117801394d7e500e804bbc127c32cccdf146f
Junaid Khan Pathan <mjunaidhira@gmail.com>
2019-10-05 20:33:13 +0600

    Added solution of problem no. 12250

---
+++ 12250---language-detection.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    string s;
+    int c = 1;
+    while (cin >> s) {
+        if (s == "#")
+            break;
+        if (s ==  "HELLO")
+            cout << "Case " << c++ << ": " << "ENGLISH" << endl;
+        else if (s ==  "HOLA")
+            cout << "Case " << c++ << ": " << "SPANISH" << endl;
+        else if (s ==  "HALLO")
+            cout << "Case " << c++ << ": " << "GERMAN" << endl;
+        else if (s ==  "BONJOUR")
+            cout << "Case " << c++ << ": " << "FRENCH" << endl;
+        else if (s ==  "CIAO")
+            cout << "Case " << c++ << ": " << "ITALIAN" << endl;
+        else if (s ==  "ZDRAVSTVUJTE")
+            cout << "Case " << c++ << ": " << "RUSSIAN" << endl;
+        else
+            cout << "Case " << c++ << ": " << "UNKNOWN" << endl;
+    }
+
+    return 0;
+}
