e71639b5421acb0fba5dfc58cafe25864e832966
Junaid Khan Pathan <mjunaidhira@gmail.com>
2019-10-08 21:32:10 +0600

    Added solution of problem no. 12531

---
+++ 12531---hours_and_minutes.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    int t;
+    while (cin >> t) {
+        if (t%6 == 0)
+            cout << "Y" << endl;
+        else
+            cout << "N" << endl;
+    }
+
+    return 0;
+}
