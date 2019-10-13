25ac32cfee118fa38521fe545ccf6dd81704aa06
Junaid Khan Pathan <mjunaidhira@gmail.com>
2019-10-13 22:55:51 +0600

    Added solution of problem no. 488

---
+++ 488---triangle_wave.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, f;
+        cin >> a >> f;
+        while (f--) {
+            for (int i = 1; i <= a; i++) {
+                for (int j = 1; j <= i; j++) {
+                    cout << i;
+                }
+                cout << endl;
+            }
+            for (int i = a-1; i >= 1; i--) {
+                for (int j = i; j >= 1; j--) {
+                    cout << i;
+                }
+                cout << endl;
+            }
+            if (t || f)
+                cout << endl;
+        }
+    }
+
+    return 0;
+}
